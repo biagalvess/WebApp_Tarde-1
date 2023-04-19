1c0b251ebc7529fc30d8bf364b4592dc3dee604a
mariacandido-droid <maria.candido3@fatec.sp.gov.br>
2023-04-19 15:58:11 -0300

    Botoes de excluir e editar cadastro de cliente

--- WebApp_Tarde/Controllers/ClientesController.cs
+++ WebApp_Tarde/Controllers/ClientesController.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using WebApp_Tarde.Models;
 
 namespace WebApp_Tarde.Controllers
@@ -14,6 +13,49 @@ namespace WebApp_Tarde.Controllers
         public IActionResult Cadastro()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SalvarDados(ClientesViewModel dados)
+        {
+            if(dados.Id == 0)
+            {
+                Random rand = new Random();
+                int numeroAleatorio = rand.Next(1, 9999);
+                dados.Id = numeroAleatorio;
+                db.Add(dados);
+            }
+            else
+            {
+                int indice = db.FindIndex(a => a.Id == dados.Id);
+                db[indice] = dados;
+
+            }
+            return RedirectToAction("Lista");
+        }
+
+        public IActionResult Excluir(int id)
+        {
+            ClientesViewModel cliente = db.Find(a => a.Id == id);
+            if(cliente != null)
+            {
+                db.Remove(cliente);
+            }
+            return RedirectToAction("Lista");
+        }
+
+        public IActionResult Editar(int id)
+        {
+            ClientesViewModel cliente = db.Find(a => a.Id == id);
+            if(cliente != null)
+            {
+                return View(cliente);
+            }
+            else
+            {
+                return RedirectToAction("Lista");  
+            }
+            
         }
     }
 }
